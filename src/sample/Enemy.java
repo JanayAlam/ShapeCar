32cfc8068fe3ac2ab9a80c3c09df8650fc008c40
Janay Alam <56800074+JanayAlam@users.noreply.github.com>
2019-11-27 02:02:42 +0600

    Updated

--- src/sample/Enemy.java
+++ src/sample/Enemy.java
@@ -4,34 +4,54 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Enemy extends Rectangle {
-    double n=0.00001;
-    public Enemy(){
+    double n = 0.00001;
+
+    public Enemy() {
         super(100, 130, Paint.valueOf("#FFFFFF"));
     }
-    public void placementY(){
-        if(n<=4){
-            if(getY()>=1000){
+
+    public void placementY() {
+        if (n <= 4) {
+            if (getY() >= 1000) {
                 setY(-250);
-            }else{
-                setY(getY()+(2+n));
+            } else {
+                setY(getY() + (2 + n));
             }
-            n+=0.00001;
-        }else{
-            if(getY()>=1000){
+            n += 0.00001;
+        } else {
+            if (getY() >= 1000) {
                 setY(-400);
-            }else{
-                setY(getY()+(2+n));
+            } else {
+                setY(getY() + (2 + n));
             }
-            n+=0.000015;
+            n += 0.000015;
         }
         if(getX()==90.75 && getY()>1000){
-            setX(321.25);
+            setX(558.5);
         }else if(getX()==321.25 && getY()>1000){
             setX(798);
         }else if(getX()==558.5 && getY()>1000){
             setX(90.75);
         }else if(getX()==798 && getY()>1000){
-            setX(558.5);
+            setX(321.25);
         }
+
+        /*int max = 4;
+        int min = 1;
+        int range = max - min + 1;
+        for (int i = 0; i < 10; i++) {
+            int rand = (int) (Math.random() * range) + min;
+            if(getY()>1000){
+                if (rand == 1) {
+                    setX(321.25);
+                } else if (rand == 2) {
+                    setX(798);
+                } else if (rand == 3) {
+                    setX(90.75);
+                } else if (rand == 4) {
+                    setX(558.5);
+                }
+            }
+        }*/
     }
 }
